09982ce50e2d9045c56c57d6eb128c078778fb2f
matejbiciste3-a11y <matejbiciste3@gmail.com>
2026-02-27 21:56:10 +0100

    Fix: refactor mobile toggle to use Icons components for reliability

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -89,15 +89,11 @@ export default function Navbar() {
                         onClick={() => setIsMenuOpen(!isMenuOpen)}
                         aria-label="Menu"
                     >
-                        <svg className="icon-menu" viewBox="0 0 24 24">
-                            <line x1="3" y1="6" x2="21" y2="6" />
-                            <line x1="3" y1="12" x2="21" y2="12" />
-                            <line x1="3" y1="18" x2="21" y2="18" />
-                        </svg>
-                        <svg className="icon-close" viewBox="0 0 24 24">
-                            <line x1="18" y1="6" x2="6" y2="18" />
-                            <line x1="6" y1="6" x2="18" y2="18" />
-                        </svg>
+                        {isMenuOpen ? (
+                            <Icons.X className="w-6 h-6 text-primary" />
+                        ) : (
+                            <Icons.Menu className="w-6 h-6 text-primary" />
+                        )}
                     </button>
                 </div>
             </div>
